a678607499cd172d9af9e651942bf1d12ed3bbf6
Tatsuhiko Arai <tatsu@tatsu.com>
2016-09-18 16:01:14 +0900

    Initial Commit

---
+++ PhotoSphereXMP/PhotoSphereXMP.h
@@ -0,0 +1,19 @@
+//
+//  PhotoSphereXMP.h
+//  PhotoSphereXMP
+//
+//  Created by Tatsuhiko Arai on 9/18/16.
+//  Copyright © 2016 Tatsuhiko Arai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoSphereXMP.
+FOUNDATION_EXPORT double PhotoSphereXMPVersionNumber;
+
+//! Project version string for PhotoSphereXMP.
+FOUNDATION_EXPORT const unsigned char PhotoSphereXMPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoSphereXMP/PublicHeader.h>
+
+
